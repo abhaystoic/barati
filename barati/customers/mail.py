537a2ec0c2fa47c735f44884efc31609dc85acda
Ishaan Chawla <ishaan.chawla@outlook.com>
2016-03-16 21:25:04 +0530

    Addition of Contact Us Form

---
+++ barati/customers/mail.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import smtplib
+
+
+def mail_send(reciever,subject,body):
+    fromaddr = settings.EMAIL_HOST_USER
+    toaddr = reciever
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP_SSL(settings.EMAIL_HOST)
+    server.login(fromaddr, settings.EMIAL_HOST_PASSWORD)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
--- barati/customers/views_cluster/contact_us.py
+++ barati/customers/views_cluster/contact_us.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response
@@ -7,9 +6,24 @@ from django.http import HttpResponse
 from customers import models as m
 import sys, json
 from dashboard import Dashboard
+from customers.forms import ContactUsForm
+from customers.mail import mail_send
 
-#@login_required(login_url='/auth/login/')
+# @login_required(login_url='/auth/login/')
 def Contact_Us(request):
-   context = RequestContext(request)
-   context_dict = {}
-   return render_to_response('customers/contact_us.html', context_dict, context_instance=context)
+    form = ContactUsForm(request.POST or None)
+    mailSuccess = 0
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        text = form.cleaned_data['text']
+        body = 'Name : ' + name + '\nEmail : ' + email + '\n' + text
+        mail_send('contactus@barati.in', 'New Contact Us Info', body)
+        mailSuccess = 1
+
+    form = ContactUsForm()
+    context = {
+        'form': form,
+        'mailSuccess': mailSuccess
+    }
+    return render(request, 'customers/contact_us.html', context)
