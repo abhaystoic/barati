1ab0abc35c60c36f6652932530c209425a91e115
kmaheshwari <a.thakur73.at@gmail.com>
2016-03-18 20:28:33 +0530

    product detail view corrected

--- barati/barati/settings.py
+++ barati/barati/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'barati',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',          # Set to empty string for default.
     }
