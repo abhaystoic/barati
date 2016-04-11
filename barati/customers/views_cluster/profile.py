eec1cf912989575041e937fefc02e5170471620b
kmaheshwari <a.thakur73.at@gmail.com>
2016-04-11 18:53:15 +0530

    class based view for profile complete

--- barati/customers/views_cluster/profile.py
+++ barati/customers/views_cluster/profile.py
@@ -21,66 +21,55 @@ class Profile(Dashboard, View):
       
       
          
-    def get_user_details(self,request, **kwargs):
+    def dispatch(self,request,*args, **kwargs):
         
-        self.user_details = m.Users.objects.get(username=request.user.username)
-        return self.user_details
+        self.us = m.Users.objects.get(username=request.user.username)
+        try:  
+            self.add = m.Address.objects.get(users_id=self.us.id)
+        except m.Address.DoesNotExist:
+            self.add=None
+        try:
+            self.rel = m.Religion.objects.get(users_id=self.us.id)
+        except m.Religion.DoesNotExist:
+            self.rel=None
+        return super(Profile, self).dispatch(request, *args, **kwargs)
 
     
 
     #@login_required(login_url='/auth/login/')
     def get(self, request, **kwargs):
-        us = self.get_user_details(request)
-        
-        try:  
-            add = m.Address.objects.get(users_id=us.id)
-        except m.Address.DoesNotExist:
-            add=None
-        try:
-            rel = m.Religion.objects.get(users_id=us.id)
-        except m.Religion.DoesNotExist:
-            rel=None
-      
-            
-        form = ProfileForm(request.POST or None,instance=us)
-        form1 = AddressForm(data=request.POST,prefix="a",instance=add)
-        form2=ReligionForm(data=request.POST,prefix="a",instance=rel)
+        form = ProfileForm(instance=self.us)
+        form1 = AddressForm(prefix="a",instance=self.add)
+        form2=ReligionForm(prefix="b",instance=self.rel)
             
         return render(request, 'customers/profile.html', {'form': form,'form1':form1,'form2':form2})
     def post(self, request, **kwargs):
-        us = self.get_user_details(request)
         
-        try:  
-            add = m.Address.objects.get(users_id=us.id)
-        except m.Address.DoesNotExist:
-            add=None
-        try:
-            rel = m.Religion.objects.get(users_id=us.id)
-        except m.Religion.DoesNotExist:
-            rel=None
-        form = ProfileForm(request.POST or None,instance=us)
-        form1 = AddressForm(data=request.POST,prefix="a",instance=add)
-        form2=ReligionForm(data=request.POST,prefix="a",instance=rel)
+        form = ProfileForm(request.POST or None,instance=self.us)
+        form1 = AddressForm(data=request.POST,prefix="a",instance=self.add)
+        form2=ReligionForm(data=request.POST,prefix="b",instance=self.rel)
         a_valid = form.is_valid()
         b_valid = form1.is_valid()
         c_valid = form2.is_valid()
-        print a_valid
+        
         if a_valid and b_valid and c_valid:
 
-            a = form.save()
-            b = form1.save(commit=False)
-            c = form2.save(commit=False)
-            b.save()
-            c.save()
-            us_add = m.Address.objects.get(id=b.id)
-            us_add.users_id = us.id
-            us_add.save(update_fields=['users_id'])
-            us_rel = m.Religion.objects.get(id=c.id)
-            us_rel.users_id =us.id
-            us_rel.save(update_fields=['users_id'])
-        return render(request, 'customers/profile.html', {'form': form,'form1':form1,'form2':form2})
-    
+            return self.form_valid(request,form,form1, form2)
+        else:
+            get(self,request)
 
+           
+    def form_valid(self,request, form,form1, form2):
 
-	
-        
+        a = form.save()
+        b = form1.save(commit=False)
+        c = form2.save(commit=False)
+        b.save()
+        c.save()
+        us_add = m.Address.objects.get(id=b.id)
+        us_add.users_id = self.us.id
+        us_add.save(update_fields=['users_id'])
+        us_rel = m.Religion.objects.get(id=c.id)
+        us_rel.users_id =self.us.id
+        us_rel.save(update_fields=['users_id'])
+        return render(request, self.template_name, {'form': form,'form1':form1,'form2':form2})
